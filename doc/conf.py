afaf345250825f9aba390948e323a470efe32d9e
alroEK <alro@nve.no>
2025-06-19 11:57:16 +0200

    Changed theme

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster" #sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 autodoc_default_options = {
